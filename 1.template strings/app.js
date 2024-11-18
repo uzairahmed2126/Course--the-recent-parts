c3761ce05f25748a886e54b82d021827a7e8cea2
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-11-18 17:05:12 +0530

    template strings

---
+++ 1.template strings/app.js
@@ -0,0 +1,4 @@
+let firstName = "John";
+let lastName = "doe";
+console.log("Name is : " + firstName + "lastName is : " + lastName); //before ES6
+console.log(`Name is : ${firstName} lastName is : ${lastName}`); //After ES6
